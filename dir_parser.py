7408a9afe70eb34a936c07960091c6e1b7f279d1
ponyii <ponyii@yandex.ru>
2020-03-17 14:13:04 +0300

    dir_parser - comment

--- dir_parser.py
+++ dir_parser.py
@@ -12,15 +12,14 @@ def merge_groups(first, second):
                 for event_type, num in second[validness][timestamp].items():
                     first[validness][timestamp][event_type] += num
 
-# ожидание новых файлов на bash - https://askubuntu.com/questions/781799/execution-permission-to-all-files-created-under-a-specific-directory-by-default/781909#781909
-# ToDO - работать с добавляемыми с директорию файлами
-# число файлов значительно превышает число CPU, так что процессу можно отдать файл целиком
+
+# число файлов значительно превышает число CPU, так что процессу можно отдать файл целиком;
 def read_dir(path, proc_n):
     pool = Pool(processes=proc_n)
     log_files = []
     for file_name in os.listdir(path):           # listdir успешно справляется с 10^6 файлов, на которых виснет ls
         log_files.append( path + file_name )     # ToDo - use pathlib
-    results = pool.map(log_parser.read_file, log_files)
+    results = pool.map(log_parser.read_file, log_files)    # ToDo - мержить нужно на лету
     for i in range(1, len(results)):
         merge_groups(results[0], results[i])
     return results[0]
